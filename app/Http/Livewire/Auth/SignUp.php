e49a50539ab4652de11a50be65b871ee722f4f88
ImamDC-MMSGI <imam.dwicaksono@mmsgroup.co.id>
2023-07-06 17:54:16 +0700

    update

--- app/Http/Livewire/Auth/SignUp.php
+++ app/Http/Livewire/Auth/SignUp.php
@@ -34,7 +34,7 @@ class SignUp extends Component
 
         auth()->login($user);
 
-        return redirect('/dashboard');
+        return redirect('/request-dashboard');
     }
 
     public function render()
